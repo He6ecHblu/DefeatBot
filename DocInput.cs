0622d22ff257ec39e5e16f26c327b0c757046cf1
He6ecHblu <43912153+He6ecHblu@users.noreply.github.com>
2022-03-24 23:06:28 +0500

    Add files via upload

---
+++ DocInput.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using DocumentFormat.OpenXml.Wordprocessing;
+using DocumentFormat.OpenXml.Packaging;
+
+namespace DefeatBot
+{
+    public class DocInput
+    {
+        private string _exeption;
+        private string _document;
+
+        public DocInput()
+        {
+            try
+            {
+                using (WordprocessingDocument doc = WordprocessingDocument.Open("/Users/he6echblu/Desktop/Глава 0.docx" as string, true))
+                {
+                    _document = doc.MainDocumentPart.Document.Body.InnerText;
+                }
+            }
+            catch (FileFormatException)
+            {
+                _exeption = "Неверно указан формат, или файл пуст";
+            }
+        }
+
+        public string GetDocument()
+        {
+            if (_document != null)
+                return _document;
+            else
+                return _exeption;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using Xceed.Words.NET;
+using Xceed.Document.NET;
+
+namespace DefeatBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DocInput document = new DocInput();
+            Console.WriteLine(document.GetDocument());
+
+            Console.WriteLine("end");
+        }
+    }
+}
